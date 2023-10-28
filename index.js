7f3c7fbc0293f35b29ad38d62609cc7fbf92ad4f
LeysanValitova <leysan.2520@bk.ru>
2023-10-28 22:57:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function message() {
+    console.log('Я учу JavaScript !')
+}
+
+message()
+
+function my_click()
+            {
+            document.getElementById("image").src="assets/images/2248.jpg";
+            };
+
+            function my_click2()
+            {
+            document.getElementById("image").src="assets/images/zv2nvcjvyje-scaled.jpg";
+            };
